dac9133ef636d41711ccdef4cceecc58f3c1ae73
ZoRo VeGa <vegamusic416@gmail.com>
2024-06-22 21:11:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,4 +38,4 @@ async def client_start():
     await idle()
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(
+    asyncio.get_event_loop().run_until_complete(client_start())
