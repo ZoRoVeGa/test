8055c3baa0d6beb5c70171b463249ee0758405ed
ZoRo VeGa <vegamusic416@gmail.com>
2024-06-22 20:39:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,33 +16,25 @@ client = Client("leomedo", API_ID, API_HASH,
                 parse_mode="html",
                 plugins=dict(root="plugins", exclude=disabled_plugins))
 
+
 async def start_client():
-    """
-    Start the client and send a startup message to the log chat.
-    """
-    # Get the restarted message and delete it
     wr = get_restarted()
     del_restarted()
-
     try:
-        # Start the client
         client.start()
+        await client.send_message(
+            log_chat,
+            f"<b>Bot started</b>\n\n<b>Version:</b> {version}",
+        )
+        logging.info(f"Bot started\nVersion: {version}")
 
-        # Send a startup message to the log chat
-        startup_message = f"<b>Bot started</b>\n\n<b>Version:</b> {version}"
-        await client.send_message(log_chat, startup_message)
-        print(startup_message)
-
-        # If the bot was restarted, edit the previous message
         if wr:
             await client.edit_message_text(wr[0], wr[1], "Restarted successfully.")
     except BadRequest as e:
         logging.warning(f"Unable to send message to log_chat: {e}")
-
-    # Start the PyTgCalls instance
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
     await pytgcalls.start()
-
-    # Idle until the bot is stopped
     await idle()
 
 if __name__ == "__main__":
